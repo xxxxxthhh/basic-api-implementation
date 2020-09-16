618daaba5b5acfe0d13877bc6e2c7e5ee145ae43
KyleXu <kyle_x@foxmail.com>
2020-09-16 21:04:45 +0800

    [XTH] implements mail empty validation

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -118,4 +118,17 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
     }
+
+    @Test
+    void can_not_register_if_mail_empty() throws Exception {
+        UserDto userDto = new UserDto("youtube", "male", 20, "", "17628282910");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDtoJson = objectMapper.writeValueAsString(userDto);
+
+        mockMvc.perform(post("/user/register")
+                .content(userDtoJson)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
+    }
 }
