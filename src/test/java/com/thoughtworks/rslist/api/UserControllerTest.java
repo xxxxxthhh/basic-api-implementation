21dbb87509d59a3621fe1eab4395acc438492a7b
KyleXu <kyle_x@foxmail.com>
2020-09-16 21:03:13 +0800

    [XTH] implements age more than 100

--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -105,4 +105,17 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isBadRequest());
     }
+
+    @Test
+    void can_not_register_if_age_more_than_100() throws Exception {
+        UserDto userDto = new UserDto("youtube", "male", 101, "alibaba@twuc.com", "17628282910");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String userDtoJson = objectMapper.writeValueAsString(userDto);
+
+        mockMvc.perform(post("/user/register")
+                .content(userDtoJson)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
+    }
 }
